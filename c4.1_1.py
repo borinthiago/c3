7e3bf2672a22a2f96d74bd89c1deb98bd1d1c39c
borinthiago <borinthiago@gmail.com>
2022-04-17 01:28:10 -0300

    functions

--- c4.1_1.py
+++ c4.1_1.py
@@ -1,7 +1,8 @@
 # Criar lista:
-nome = ["thiago", "Bruna", "Heloisa", "Gustavo"]
+nome = ["thiago", "thiago", "Bruna", "Heloisa", "Gustavo"]
 # Exibir a lista:
 print(nome)
+"""""
 # Exibir a lista por indice:
 print(nome[1])
 # Exibir item a item da lista usando loop for:
@@ -20,3 +21,30 @@ print(nome)
 nome.insert(2,input("Coloque um novo valor para 2 "))
 for x in nome:
     print(x)
+
+# Remover uma entrada (últim)
+nome.pop(0)
+for x in nome:
+    print(x)
+
+# Para remover um valor específico da lista:
+nome.remove("Heloisa")
+print(nome)
+"""""
+# Contar quantos itens há na lista:
+print("O nome thiago aparece {}x".format(nome.count("thiago")))
+# Contar o número de vezes que a string aparece na lista
+contagem = nome.count("Heloisa")
+print(contagem)
+# Colocar a lista na ordem contrária
+nome.reverse()
+print(nome)
+# Organizar em ordem alfabética
+nome.sort()
+print(nome)
+# informar o tamanho do objeto
+print(len(nome))
+
+# Criar nova lista de elementos inteiros:
+valores = [1, 2, 3, 5]
+print(sum(valores))
